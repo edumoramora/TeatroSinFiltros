484bae614764298d226a5354b49e38535bf31015
eduardo.mora.mora <eduardo.mora.mora@alumnos.upm.es>
2023-12-08 18:23:37 +0100

    #67 Configuración de un servidor, con el cual usaremos una API para realizar solicitudes.

---
+++ TeatroSinFiltros/TeatroSinFiltros/backend/server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:4200' //mismo que el del server en el que se ejecuta la app
+  }));
+app.use(express.json());
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'teatro_sin_filtros'
+});
+
+db.connect(err => {
+  if (err) { console.error('Error al conectar: ' + err.stack); return; }
+  console.log('Conectado a la base de datos con ID ' + db.threadId);
+});
+
+
+app.get('/api/obras', (req, res) => {
+    const query = 'SELECT titulo, imagen_url, descripcion FROM obras';
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error en el servidor');
+      } else {
+        res.json(results);
+      }
+    });
+});
+
+app.post('/api/login', (req, res) => {
+  const { nombre_usuario, contrasena } = req.body;
+  const query = 'SELECT * FROM usuarios WHERE nombre_usuario = ?';
+
+  db.query(query, [nombre_usuario], (err, result) => {
+      if (err) {
+          res.status(500).send('Error en el servidor');
+          return;
+      }
+
+      if (result.length > 0) {
+        console.log('Contraseña ingresada:', contrasena);
+        console.log('Hash en la base de datos:', result[0].contrasena_hash);
+        console.log("JWT Secret:", process.env.JWT_SECRET);
+
+          bcrypt.compare(contrasena, result[0].contrasena_hash, (err, isMatch) => {
+              if (err) {
+                  res.status(500).send('Error al comparar contraseñas');
+                  return;
+              }
+
+              if (isMatch) {
+                  const token = jwt.sign(
+                      { id: result[0].id, rol: result[0].rol },
+                      process.env.JWT_SECRET, 
+                      { expiresIn: '1h' }
+                  );
+                  console.log(result[0].rol);
+                  res.json({ mensaje: "Autenticación exitosa", token , rol: result[0].rol});
+              } else {
+                  res.status(401).send(result[0].contrasena_hash);
+              }
+          });
+      } else {
+          res.status(404).send('Usuario no encontrado');
+      }
+  });
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
