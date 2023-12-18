3b3296962beb85f9448a2be10f625c5d563b16bd
edumoramora <67929314+edumoramora@users.noreply.github.com>
2023-12-18 12:37:51 +0100

    Update server.js

--- TeatroSinFiltros/TeatroSinFiltros/backend/server.js
+++ TeatroSinFiltros/TeatroSinFiltros/backend/server.js
@@ -12,16 +12,20 @@ app.use(cors({
 app.use(express.json());
 
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'teatro_sin_filtros'
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-db.connect(err => {
-  if (err) { console.error('Error al conectar: ' + err.stack); return; }
-  console.log('Conectado a la base de datos con ID ' + db.threadId);
+// Conectándose a la base de datos
+con.connect(function(err) {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log("¡Conectado a la base de datos!");
 });
 
 
